9a1f54f32a7af4cf435fe2096c080c3dfe660170
yazzzna <145989666+yazzzna@users.noreply.github.com>
2024-12-03 22:52:37 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Part 1:");
+        GetUser GetUser = new GetUser();
+        GetUser.User();
+
+
+        System.out.println("\nPart 2:");
+        GetCompany GetCompany = new GetCompany();
+        GetCompany.Company();
+    }
+}
